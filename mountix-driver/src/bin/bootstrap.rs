7f645782aaa3dce9a8a633bae1e15fcf5b99484c
codemountains <4kz12zz@gmail.com>
2024-07-13 01:21:01 +0900

    :sparkles: add doc comment

--- mountix-driver/src/bin/bootstrap.rs
+++ mountix-driver/src/bin/bootstrap.rs
@@ -2,6 +2,9 @@ use mountix_driver::module::Modules;
 use mountix_driver::startup::{init_app, startup};
 use std::sync::Arc;
 
+/// main function
+///
+/// main 関数
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     init_app();
