c57c62922b88d544ec776be7f6626a4069b5f7b5
Kazuno Fukuda <4kz12zz@gmail.com>
2025-06-21 23:28:57 +0900

    :technologist: Fix migration scripts

--- migrations/devcontainer/migrate-in-docker.sh
+++ migrations/devcontainer/migrate-in-docker.sh
@@ -10,4 +10,13 @@ mongoimport \
   --collection=mountains \
   --file="/docker-entrypoint-initdb.d/mountix_db-mountains.json"
 
+echo "Creating geospatial index on mountains collection..."
+
+mongosh \
+  --username="$MONGO_INITDB_ROOT_USERNAME" \
+  --password="$MONGO_INITDB_ROOT_PASSWORD" \
+  --authenticationDatabase=admin \
+  mountix_db \
+  --eval "db.mountains.createIndex({ location: '2dsphere' });"
+
 echo "Completed migration"
--- migrations/migrate.sh
+++ migrations/migrate.sh
@@ -5,10 +5,23 @@ source ./.env
 brew tap mongodb/brew
 brew install mongodb-database-tools
 
+echo "Migrating data into MongoDB..."
+
 mongoimport $DATABASE_URL \
---collection=mountains \
---db=mountix_db \
---file="./data/mountix_db-mountains.json"
+  --collection=mountains \
+  --db=mountix_db \
+  --file="./data/mountix_db-mountains.json"
+
+echo "Creating geospatial index on mountains collection..."
+
+mongosh \
+  --username="$MONGO_INITDB_ROOT_USERNAME" \
+  --password="$MONGO_INITDB_ROOT_PASSWORD" \
+  --authenticationDatabase=admin \
+  mountix_db \
+  --eval "db.mountains.createIndex({ location: '2dsphere' });"
+
+echo "Completed migration"
 
 brew remove mongodb-database-tools
 brew untap mongodb/brew
